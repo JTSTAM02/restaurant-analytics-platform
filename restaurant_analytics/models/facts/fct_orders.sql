6b2b33e6c717ee69ef680765f6bc6fd04cdf79ef
Tyler Stamm <tyler.stamm1313@gmail.com>
2026-09-11 21:34:14 -0400

    Updated fct_orders with error handling for schema changes

--- restaurant_analytics/models/facts/fct_orders.sql
+++ restaurant_analytics/models/facts/fct_orders.sql
@@ -1,7 +1,8 @@
 {{ config
 	(materialized='incremental',
 	unique_key='order_id',		
-        incremental_strategy='merge'
+        incremental_strategy='merge',
+        on_schema_change='append_new_columns'
 ) }}
 
 SELECT
